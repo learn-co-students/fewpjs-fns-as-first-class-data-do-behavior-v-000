a843b8df60cffbc099659a6e53062234fb40863f
Rebecca Tong <rtong@buffalo.edu>
2019-10-08 00:40:33 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  let timex = time.split(time[0,2])
+  let number = parseInt(timex,10)
+  if (number < 12) {
+    return "Good Morning"
+  }
+  else if (number >= 12 && number <= 17) {
+   return "Good Afternoon"
+  }
+  else if (number > 17){
+    return "Good Evening"
+  }
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(text){
+  // const newGreeting = document.querySelector('greeting')
+  let content = document.getElementById("greeting")
+  greeting.innerText = text
+
+}
