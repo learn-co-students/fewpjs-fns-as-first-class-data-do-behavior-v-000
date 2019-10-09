3df9c875f62ffab8b779522147942e9407c07a70
Sushi Chanrai <sushi@pathworks.co>
2019-10-09 16:06:23 -0400

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,30 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  const timeSplit = timeString.split(":", 1);
+  const parsed = parseInt(timeSplit, 10)
+  if (parsed >= 6 && parsed < 12 ) {
+    return "Good Morning";
+  } else if (parsed >= 12 && parsed < 17){
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+// it should update the text inside the #greeting node with the content of the first argument
+//use innerhtml to do this
+
+  function displayMessage(content){
+    let element = document.querySelector("#greeting");
+     element.innerText = `${content}`;
+    //return element;
+  }
