7ea59f8178aabe83e8e05d422f4f67c9d1af9248
dani8439 <dani8439@aol.com>
2019-11-12 12:53:54 -0500

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ function greet(timeStr) {
   const hour = parseInt(timeStr, 10);
 
   if (hour < 12) return "Good Morning"
-  if (hour > 17 ) return "Good Evening"
+  if (hour > 17) return "Good Evening"
   return "Good Afternoon"
 
 }
+
+// const int = parseInt("12:00", 10)
+// => returns 12. parseInt will convert string num by base number.  
 
 /* Write your implementation of displayMessage() */
 
