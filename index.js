54568ef4facda7b4e7929bd8bd5b43cefb88453e
John Blank <veloblank@gmail.com>
2020-01-03 05:43:50 -0500

    Refactor code. Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-	const timeString = document.getElementById("time").value;
-	displayMessage(greet(timeString));
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 function greet(time) {
-	let hour = time.split(":")[0];
+  let hour = time.split(":")[0];
 
-	if (hour < 12) {
-		return "Good Morning";
-	} else if (hour < 17) {
-		return "Good Afternoon";
-	} else {
-		return "Good Evening";
-	}
+  if (hour < 12) {
+    return displayMessage("Good Morning");
+  } else if (hour < 17) {
+    return displayMessage("Good Afternoon");
+  } else {
+    return displayMessage("Good Evening");
+  }
 }
 
 function displayMessage(greeting) {
-	document.getElementById("greeting").innerText = greeting;
+  return (document.getElementById("greeting").innerText = greeting);
 }
