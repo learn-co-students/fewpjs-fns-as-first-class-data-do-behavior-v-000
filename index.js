ae119b52e93d3dada9f0757bcdf1bd5215642397
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2020-01-20 23:20:10 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let splitTime = timeString.split(":")
+  let hour = parseInt(splitTime[0])
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon"
+  } else if (hour > 17 && hour <= 24) {
+    return "Good Evening"
+  } else {
+    return timeString
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  let element = document.getElementById("greeting")
+  element.innerText = message
+}
