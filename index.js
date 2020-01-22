d09c6d3fd1bfe983f103cc2c579aa8f36b1e749d
William Barela <william.barela.dev@gmail.com>
2020-01-22 22:08:42 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let [hour, ] = timeString.split(separator=":");
+  let greeting;
+  hour = parseInt(hour);
+  
+  if(hour < 12){
+    greeting = "Good Morning";  
+  } else if(hour > 15){
+    greeting = "Good Evening";
+  } else {
+    greeting = "Good Afternoon";
+  }
+  
+  return(greeting);
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+ let greeting = document.querySelector('h1#greeting');
+ greeting.innerHTML = message;
+}
