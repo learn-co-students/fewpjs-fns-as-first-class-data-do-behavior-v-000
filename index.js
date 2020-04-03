4ccd1870c862c72412789a21be21d1fadc55104a
benitolsantos <github email address>
2020-04-02 21:27:22 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(timeInput) {
+  var timeArray = timeInput.split(':');
+  var timeByHour = parseInt(timeArray[0]);
+  var timeByMinute = parseInt(timeArray[1]);
+
+  if (timeByHour < 12) {
+    return "Good Morning"
+  }
+
+  else(timeByHour >= 12); {
+    if (timeByHour < 17) {
+      return "Good Afternoon";
+    }
+
+    else if (timeByHour >= 17) {
+      return "Good Evening";
+    }
+  }
+}
+
+function displayMessage(textThing) {
+  let newMessage = document.querySelector('h1#greeting'); //Create element
+  newMessage.id = `displaythis`; //Add id
+  newMessage.innerHTML = `${textThing}`; //Text
+  console.log(newMessage)
+  document.body.appendChild(newMessage); //Add to html file
+}
