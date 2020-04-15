04a7169c41cdc6a4408cb7589b4fa6133b89a989
sdcro <sdcrouse91@gmail.com>
2020-04-15 15:09:18 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(militaryTimeString) {
+  const splitTime = militaryTimeString.split(":");
+  const hour = parseInt(splitTime[0]);
+
+  if (hour < 12) { return "Good Morning"; }
+  if (hour >= 12 && hour < 17) { return "Good Afternoon"; } // Interesting; I can't do 12 <= hour < 17.
+  return "Good Evening";
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  const greetingHeader = document.getElementById('greeting');
+  greetingHeader.innerText = message; // This works with TextContent, too, but somehow it won't pass the tests.
+}
