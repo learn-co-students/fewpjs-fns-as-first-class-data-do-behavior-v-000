287bf06944bcfd3042a31cf0a77dbacfe67cbc5a
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-23 10:59:02 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  // split time into hours and minutes
+  const section = timeString.split(':');
+  // find hours and convert hours into integer
+  const hours = parseInt(section[0]);
+  // if statement for time
+  if (hours < 12) {
+    return "Good Morning";
+  } else if (hours < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
