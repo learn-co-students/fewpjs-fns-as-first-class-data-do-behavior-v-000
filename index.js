d00725f87b4013c605e72aebb0b8f3b7bc2fa904
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 22:13:52 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const time = timeString.split(':');
+  const hour = parseInt(time[0], 10);
+
+  if (hour < 12) {
+    return 'Good Morning';
+  } else if (hour >= 12 && hour <= 17) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+// When the function runs it should update the text inside the #greeting node with the content of the argument.
+// It does not return anything.
+// <h1 id="greeting"></h1>
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
