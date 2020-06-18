da6c2ca4419228485e1991865b2557446b848af4
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-18 02:38:39 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(phrase) {
+  const time = parseInt(phrase)
+  console.log(time)
+
+  if (time < 12) {
+    return "Good Morning";
+  } 
+  if (12 <= time && time <= 17) {
+    return "Good Afternoon";
+  } else {
+    return 'Good Evening';
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
