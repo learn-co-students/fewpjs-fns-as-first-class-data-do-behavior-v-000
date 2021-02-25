85c21e77be97b3fca4fc915448a2ff4742d44b69
github username <d24dust@hotmail.com>
2021-02-25 11:04:57 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,29 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  // "HH:MM"
+  const timeArr = parseArrayToInt(time.split(":")) 
+  // Will return array as [HH, MM]
+
+  if (timeArr[0] >= 17) {
+    return "Good Evening"
+  } else if (timeArr[0] >= 12 && timeArr[0] < 17) {
+    return "Good Afternoon"
+  } else if (timeArr[0] < 12) {
+    return "Good Morning"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg
+}
+
+function parseArrayToInt(array) {
+  const rtnArray = []
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    rtnArray.push(parseInt(element))
+  }
+  return rtnArray
+}
