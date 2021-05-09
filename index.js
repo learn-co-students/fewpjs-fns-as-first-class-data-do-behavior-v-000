4a864ff95b89c7d98f077b2fbf7b8f9226e7e783
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-08 19:53:23 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const timeArray = timeString.split(":");
+  console.log(timeArray);
+  if (parseInt(timeArray[0]) >=0 && parseInt(timeArray[0]) < 12) {
+    return "Good Morning";
+  }
+  else if (parseInt(timeArray[0]) >= 12 && parseInt(timeArray[0]) <= 17){
+      return "Good Afternoon";
+    }
+  else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting) {
+  let element = document.getElementById("greeting");
+  element.innerText = greeting;
+}
